24d0222f232bf9c23bb87798a92f7dfa8c9b0d28
Marco <sejereje1011@gmail.com>
2025-01-22 10:57:12 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,3 @@
 CONFIG = {
-    "n": 200,
-    "true_state_means": [2, -2],
-    "true_state_stds": [1, 1.5],
-    "p_switch": 0.1
+    "time_series_path": "time_series.csv",  # Path to the time series data file
 }
